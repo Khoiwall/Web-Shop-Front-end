b8890b707d1ec57c1fc9afb5a7fb955bb31a7ebf
khoiwall <khoitr190@gmail.com>
2020-03-13 11:29:11 +0700

    add file js

---
+++ ratingStart.js
@@ -0,0 +1,77 @@
+function one(){
+    document.getElementById('starOne').style.color = 'red';
+}
+function two(){
+    document.getElementById('starOne').style.color = 'red';
+    document.getElementById('starTwo').style.color = 'red';
+}
+function three(){
+    document.getElementById('starOne').style.color = 'red';
+    document.getElementById('starTwo').style.color = 'red';
+    document.getElementById('starThree').style.color = 'red';
+}
+function four(){
+    document.getElementById('starOne').style.color = 'red';
+    document.getElementById('starTwo').style.color = 'red';
+    document.getElementById('starThree').style.color = 'red';
+    document.getElementById('starFour').style.color = 'red';
+}
+function five(){
+    document.getElementById('starOne').style.color = 'red';
+    document.getElementById('starTwo').style.color = 'red';
+    document.getElementById('starThree').style.color = 'red';
+    document.getElementById('starFour').style.color = 'red';
+    document.getElementById('starFive').style.color = 'red';
+}
+//
+function six(){
+    document.getElementById('starSix').style.color = 'red';
+}
+function sevent(){
+    document.getElementById('starSix').style.color = 'red';
+    document.getElementById('starSevent').style.color = 'red';
+}
+function eight(){
+    document.getElementById('starSix').style.color = 'red';
+    document.getElementById('starSevent').style.color = 'red';
+    document.getElementById('starEight').style.color = 'red';
+}
+function night(){
+    document.getElementById('starSix').style.color = 'red';
+    document.getElementById('starSenvent').style.color = 'red';
+    document.getElementById('starEight').style.color = 'red';
+    document.getElementById('starNight').style.color = 'red';
+}
+function ten(){
+    document.getElementById('starSix').style.color = 'red';
+    document.getElementById('starSevent').style.color = 'red';
+    document.getElementById('starEight').style.color = 'red';
+    document.getElementById('starNight').style.color = 'red';
+    document.getElementById('starTen').style.color = 'red';
+}
+//
+function one1(){
+    document.getElementById('star1').style.color = 'red';
+}
+function two1(){
+    document.getElementById('star1').style.color = 'red';
+    document.getElementById('star2').style.color = 'red';
+}
+function three1(){
+    document.getElementById('star1').style.color = 'red';
+    document.getElementById('star2').style.color = 'red';
+    document.getElementById('star3').style.color = 'red';
+}
+function four1(){
+    document.getElementById('star1').style.color = 'red';
+    document.getElementById('star2').style.color = 'red';
+    document.getElementById('star3').style.color = 'red';
+    document.getElementById('star4').style.color = 'red';
+}
+function five1(){
+    document.getElementById('star1').style.color = 'red';
+    document.getElementById('star2').style.color = 'red';
+    document.getElementById('star3').style.color = 'red';
+    document.getElementById('star4').style.color = 'red';
+    document.getElementById('star5').style.color = 'red';
+}
